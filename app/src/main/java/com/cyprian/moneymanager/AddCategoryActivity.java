68c7f77531aff2a6bea335c0e7ecda6056b478d8
nkanyi73 <nkanyi73@gmail.com>
2021-09-14 12:04:49 +0300

    Set up the Database

--- app/src/main/java/com/cyprian/moneymanager/AddCategoryActivity.java
+++ app/src/main/java/com/cyprian/moneymanager/AddCategoryActivity.java
@@ -6,8 +6,20 @@ import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AddCategoryActivity extends AppCompatActivity {
+    //declaring an EditText to hold the Category value
+    private EditText addCat;
+    private Button addBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +28,22 @@ public class AddCategoryActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
+        //Initializing the EditText
+        addCat = findViewById(R.id.input_category);
+        addBtn = findViewById(R.id.btn_add_category);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String category = addCat.getText().toString();
+                if(category.isEmpty()){
+                    Toast.makeText(AddCategoryActivity.this,"No Category Added",Toast.LENGTH_SHORT).show();
+                }else{
+                   // FirebaseDatabase.getInstance().getReference().child("users").push().child("Categories").setValue(category);
+                }
+            }
+        });
 
     }
 
